919a83027bdf9fb5db4517d278634cc45e5f40b6
Your Name <you@example.com>
2022-09-03 12:33:20 +0300

    исправлено

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 function whenButtonClicked(){
 
-
+    let selectAction = document.getElementById("select");
+    console.log(selectAction.value)
+    
     let value1 = document.getElementById("input1").value;
     let value2 = document.getElementById("input2").value;
-    let selectAction = document.getElementById("select").value;
+    
     var result;
     switch (selectAction.value){
         case '1':
@@ -23,6 +25,7 @@ function whenButtonClicked(){
 
     }
 
-    document.getElementById("results").innerText = result;
     console.log(value1,value2);
+    document.getElementById("results").innerText = result;
+    
 }
